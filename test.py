cd1e75e73b89438a237af51f5e3ea42da05cc81a
jchan1992 <justin.chan.92@live.com>
2017-08-14 23:56:19 +1000

    building my first pygame game

---
+++ test.py
@@ -0,0 +1,47 @@
+# import pygame
+import test2
+from test2 import *                
+car11 = test2.car1()
+car22 = test2.car2()
+
+pygame.init()
+
+clock = pygame.time.Clock()
+display = pygame.display.set_mode((displayw,displayh))
+pygame.display.set_caption("My first game")
+
+pygame.mixer.music.load('initiald.mid')
+pygame.mixer.music.play(-1)	
+
+end_game = False
+
+while not end_game:
+        display.fill(black)
+        pygame.draw.lines(display,yellow,False,[(displayw/4-2,0),(displayw/4-2,displayh)],4)
+        pygame.draw.lines(display,yellow,False,[(displayw/2-2,0),(displayw/2-2,displayh)],4)
+        pygame.draw.lines(display,yellow,False,[(3*displayw/4-2,0),(3*displayw/4-2,displayh)],4)
+
+        display.blit(car11.image,car11.loc)
+        display.blit(car22.image,car22.loc)
+        for event in pygame.event.get():
+                print(event)
+                if event.type == pygame.QUIT:
+                        end_game = True
+                if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_q:
+                                end_game = True
+                        if event.key == pygame.K_LEFT:
+                                car11.update()
+                                display.blit(car11.image,car11.loc)
+                        if event.key == pygame.K_RIGHT:
+                                car22.update()
+                                display.blit(car22.image,car22.loc)
+                
+        clock.tick(1000)
+        pygame.display.update()
+        
+
+pygame.mixer.music.stop()
+pygame.quit()
+sys.ext()
+quit()
---
+++ test2.py
@@ -0,0 +1,61 @@
+import pygame
+import sys
+
+#set pos
+LEFT = 0
+RIGHT = 1
+
+#set values
+displayw = 600
+displayh = 800
+
+#set colours
+black = (0,0,0)
+white = (255,255,255)
+yellow = (255,255,0)
+
+class car1(pygame.sprite.Sprite):
+        def __init__(self):
+                pygame.sprite.Sprite.__init__(self)
+                self.pos = LEFT
+                self.x = 20
+                self.y = displayh - 100
+                self.loc = (self.x,self.y)
+                self.image = pygame.image.load('car1.jpg')
+                self.rect = self.image.get_rect()
+                print(self.x,'\t',self.y,'\t',self.rect)
+
+        def update(self):
+                self.pos = self.pos^1
+                if self.pos == LEFT:
+                        self.x = 20
+                        self.y = displayh - 100
+                        self.loc = (self.x,self.y)
+                else:
+                        self.x = displayw/4+20
+                        self.y = displayh - 100
+                        self.loc = (self.x,self.y)
+                print(self.x,'\t',self.y,'\t',self.rect)
+
+class car2(pygame.sprite.Sprite):
+        def __init__(self):
+                pygame.sprite.Sprite.__init__(self)
+                self.pos = RIGHT
+                self.x = displayw*3/4+20
+                self.y = displayh - 100
+                self.loc = (self.x,self.y)
+                self.image = pygame.image.load('car2.jpg')
+                self.rect = self.image.get_rect()
+                print(self.x,'\t',self.y,'\t',self.rect)
+
+        def update(self):
+                self.pos = self.pos^1
+                if self.pos == RIGHT:
+                        self.x = displayw*3/4+20
+                        self.y = displayh - 100
+                        self.loc = (self.x,self.y)
+                else:
+                        self.x = displayw/2+20
+                        self.y = displayh - 100
+                        self.loc = (self.x,self.y)
+                print(self.x,'\t',self.y,'\t',self.rect)
